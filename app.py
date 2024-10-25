a8e0006810bcfa91874dbcd4adfb577d51130e9d
gamingbee48 <129998281+gamingbee48@users.noreply.github.com>
2024-10-25 23:56:01 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Route für die Homepage
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Socket.IO-Ereignis für das Senden der Position und Richtung
+@socketio.on('update_position')
+def handle_position(data):
+    print(f'Position erhalten: {data}')
+    # Sendet die Positionsdaten an alle verbundenen Clients
+    socketio.emit('update_map', data, namespace='/')
+
+if __name__ == '__main__':
+    socketio.run(app)
